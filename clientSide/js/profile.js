8b87497d69b4907aece1eb24dc74d472580b45f5
OfekZavaro <132827073+OfekZavaro@users.noreply.github.com>
2023-08-23 12:00:09 +0300

    profile page not final

--- clientSide/js/profile.js
+++ clientSide/js/profile.js
@@ -1,15 +1,43 @@
 $(document).ready(function() {
-    // Fetch user profile information from the server using AJAX
-    $.ajax({
-        url: '/api/user-profile', // Update the URL to your API endpoint
-        method: 'GET',
-        success: function(data) {
-            $('#nameBox').text(data.name);
-            $('#emailBox').text(data.email);
-            $('#roleBox').text(data.roles.join(', ')); // Assuming roles is an array
-        },
-        error: function() {
-            console.log('Error fetching user profile information.');
-        }
+    // Handler for User Info button click
+    $('#userInfoBtn').click(function() {
+        var token = localStorage.getItem("token");
+        var userId = localStorage.getItem("userId"); // Get the user ID from local storage
+
+        $.ajax({
+            url: `http://localhost:3000/account/${userId}`, // Use the user ID to construct the URL
+            method: 'GET',
+            headers: {
+                Authorization: `Bearer ${token}`
+            },
+            success: function(data) {
+                var userInfoHtml = "<div class='card-header'>User Information</div>";
+                userInfoHtml += "<div class='card-body'>";
+                userInfoHtml += "<div class='profile-box'>";
+                userInfoHtml += "<label for='name'>Name:</label>";
+                userInfoHtml += "<div>" + data.name + "</div>";
+                userInfoHtml += "</div>";
+                userInfoHtml += "<div class='profile-box'>";
+                userInfoHtml += "<label for='email'>Email:</label>";
+                userInfoHtml += "<div>" + data.email + "</div>";
+                userInfoHtml += "</div>";
+                userInfoHtml += "<div class='profile-box'>";
+                userInfoHtml += "<label for='role'>Role:</label>";
+                userInfoHtml += "<div>" + data.roles.join(', ') + "</div>";
+                userInfoHtml += "</div>";
+                userInfoHtml += "</div>";
+                userInfoHtml += "</div>";
+                
+                $('#userInfoContainer').html(userInfoHtml);
+            },
+            error: function() {
+                console.log('Error fetching user profile information.');
+            }
+        });    
     });
 });
+
+
+
+
+
