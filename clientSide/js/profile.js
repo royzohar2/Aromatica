9966ed148966c1430e6bd393345c0969792a98b2
Roy Zohar <royzohar2@gmail.com>
2023-08-26 20:38:22 +0300

    add currect profile css and js

--- clientSide/js/profile.js
+++ clientSide/js/profile.js
@@ -1,11 +1,21 @@
+function parseJwt(token) {
+    const base64Url = token.split('.')[1];
+    const base64 = base64Url.replace('-', '+').replace('_', '/');
+    return JSON.parse(atob(base64));
+  }
+
+
 $(document).ready(function() {
-    // Handler for User Info button click
     $('#userInfoBtn').click(function() {
         var token = localStorage.getItem("token");
-        var userId = localStorage.getItem("userId"); // Get the user ID from local storage
+        console.log(token);
+        const decodedToken = parseJwt(token);
+        console.log('Decoded Token:', decodedToken);
+        var userId = decodedToken.userId;
+        console.log(userId);
 
         $.ajax({
-            url: `http://localhost:3000/account/${userId}`, // Use the user ID to construct the URL
+            url: `http://localhost:3000/account/${userId}`,
             method: 'GET',
             headers: {
                 Authorization: `Bearer ${token}`
@@ -35,9 +45,77 @@ $(document).ready(function() {
             }
         });    
     });
+
+    $(document).ready(function() {
+        $('#orderHistoryBtn').click(function() {
+            var token = localStorage.getItem("token");
+            const decodedToken = parseJwt(token);
+            var userId = decodedToken.userId;
+    
+            $.ajax({
+                url: `http://localhost:3000/account/${userId}`,
+                method: 'GET',
+                headers: {
+                    Authorization: `Bearer ${token}`
+                },
+                success: function(userData) {
+                    var userOrders = userData.orders;
+    
+                    var orderTable = $("<table class='order-table'>");
+                    orderTable.append("<tr><th>Order ID</th><th>Order Date</th><th>Products</th><th>Total Amount</th></tr>");
+    
+                    userOrders.forEach(function(orderId) {
+                        $.ajax({
+                            url: `http://localhost:3000/order/${orderId}`,
+                            method: 'GET',
+                            headers: {
+                                Authorization: `Bearer ${token}`
+                            },
+                            success: function(orderData) {
+                                // Fetch product details for each product in the order
+                                var productPromises = orderData.products.map(function(productId) {
+                                    return $.ajax({
+                                        url: `http://localhost:3000/perfumes/${productId}`,
+                                        method: 'GET',
+                                        headers: {
+                                            Authorization: `Bearer ${token}`
+                                        }
+                                    });
+                                });
+    
+                                Promise.all(productPromises)
+                                    .then(function(products) {
+                                        var productNames = products.map(function(product) {
+                                            return product.name;
+                                        }).join(", ");
+    
+                                        var orderRow = $("<tr>");
+                                        orderRow.append("<td>" + orderData._id + "</td>");
+                                        orderRow.append("<td>" + new Date(orderData.orderDate).toLocaleString() + "</td>");
+                                        orderRow.append("<td>" + productNames + "</td>");
+                                        orderRow.append("<td>" + orderData.totalAmount + "</td>");
+    
+                                        orderTable.append(orderRow);
+                                    })
+                                    .catch(function(error) {
+                                        console.log('Error fetching product details:', error);
+                                    });
+                            },
+                            error: function() {
+                                console.log('Error fetching order details.');
+                            }
+                        });
+                    });
+    
+                    // Append the order table to the userInfoContainer
+                    $('#userInfoContainer').html(orderTable);
+                },
+                error: function() {
+                    console.log('Error fetching user information.');
+                }
+            });
+        });
+    });
+    
+    
 });
-
-
-
-
-
