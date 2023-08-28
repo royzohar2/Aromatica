b982fb97c8847949fff33b0a9f71ae62dcab7590
Adi Mor <adimor87@gmail.com>
2023-08-28 10:30:16 +0300

    adding comments

--- clientSide/js/index.js
+++ clientSide/js/index.js
@@ -1,11 +1,11 @@
 (() => {
   $(document).ready(function () {
-    console.log("document ready index");
+    //console.log("document ready index");
 
-    setDialogHandler();
+    setDialogHandler(); // Attach the function to the form's submit event
     setNavBarHandler();
     updateAuthButtons(); // Update the authentication buttons on page load
-    setSearchHandler();
+    setSearchHandler(); // Function to attach event handler to search input field
 
     // Load the initial content (Home page)
     const correntUrl = window.location.hash.slice(1).split("?"); // Remove the '#'
@@ -14,21 +14,23 @@
     router.navigateTo(page ? page : "home", params);
   });
 
+  // function sets up event handlers to handle navigation in two scenarios:
+  // when the user clicks on links in the navigation bar and when the URL hash changes due to browser back/forward navigation
   function setNavBarHandler() {
     $("nav a").on("click", async function (event) {
-      event.preventDefault();
+      event.preventDefault(); // Prevent the default link behavior (full page reload)
       const pageName = $(this).attr("href");
       await router.navigateTo(pageName);
     });
-
+    // Listen for changes in the URL hash (for back/forward navigation)
     window.addEventListener("hashchange", async function () {
       const correntUrl = window.location.hash.slice(1).split("?"); // Remove the '#'
-      const page = correntUrl?.[0];
-      const params = correntUrl?.[1];
-      await router.navigateTo(page, params);
+      const page = correntUrl?.[0]; // Extract the page name from the hash
+      const params = correntUrl?.[1]; // Extract any parameters from the hash
+      await router.navigateTo(page, params); // Navigate to the specified page with parameters
     });
   }
-
+  // Function to attach event handler to search input field
   function setSearchHandler() {
     $("#search-input").on("keyup", async function (event) {
       const search = $(this).val();
@@ -36,7 +38,7 @@
       debounce(() => searchProducts(search), 500);
     });
   }
-
+  // Function to fetch and display search results in a modal
   async function searchProducts(search) {
     try {
       const response = await $.ajax({
@@ -48,7 +50,6 @@
       updateSearchResults(response);
       // Open the modal
       $("#searchResultsModal").modal("show");
-      //return response;
     } catch (error) {
       console.error("Error fetching data:", error);
     }
@@ -161,16 +162,18 @@
       $("#logoutButton").hide();
     }
   }
-
+  // Function to update the search results modal with fetched data
   function updateSearchResults(results) {
+    // Clear previous search results
     const searchResultsContainer = $("#searchResults");
     searchResultsContainer.empty();
 
     if (results.length === 0) {
+      // Display "No results found" message
       searchResultsContainer.html("<p>No results found</p>");
       return;
     }
-
+    // Create a list of search result items
     const resultList = $("<ul class='list-group'></ul>");
 
     results.forEach((result) => {
@@ -187,7 +190,7 @@
       </li>`;
       resultList.append(listItem);
     });
-
+    // Append the list of search result items to the container
     searchResultsContainer.append(resultList);
   }
 
@@ -199,22 +202,24 @@
     router.navigateTo("home");
   });
 
+  // Debounce function for delaying search requests
+  // helps to delay search requests to avoid excessive server calls.
   let searchTimeout;
   function debounce(func, delay) {
     clearTimeout(searchTimeout);
     searchTimeout = setTimeout(func, delay);
   }
 })();
-
+// Router module for handling navigation and loading content
 const router = (() => {
   let correntPage;
   let correntParams;
-
+  // Function to navigate to a specific page
   async function navigateTo(page, params) {
     if (correntPage === page && correntParams === params) {
       return;
     }
-
+    // Fetch and display content of the requested page
     const url = `/clientSide/pages/${page}.html`;
     try {
       const response = await fetch(url);
@@ -223,7 +228,7 @@ const router = (() => {
       }
       const content = await response.text();
       $("main#content").html(content);
-
+      // Update current page and params, and update URL hash
       correntPage = page;
       correntParams = params;
       const urlParams = params ? "?" + params : "";
@@ -232,6 +237,7 @@ const router = (() => {
       console.error("Error loading content:", error);
     }
   }
+  // Function to get the current params
   function getParams() {
     return correntParams;
   }
--- serverSide/middlewares/auth-middleware.js
+++ serverSide/middlewares/auth-middleware.js
@@ -1,27 +1,37 @@
-// roleAuth.js
+// Import required dependencies and models
 jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
+
+// Get JWT_SECRET from environment variables
 const JWT_SECRET = process.env.jwtSecret;
 
+// Middleware function to check if the user has the required role
 function requireRole(role) {
   return async function (req, res, next) {
+    // Get the JWT token from the Authorization header
     const authHeader = req.header("Authorization");
     const token = authHeader?.split(" ")[1];
+    // If no token is provided, return Unauthorized
     if (!token) {
       return res.status(401).json({ error: "Unauthorized" });
     }
 
     try {
+      // Verify the token using the JWT_SECRET
       const decoded = jwt.verify(token, JWT_SECRET);
+      // Find the user by userId from the decoded token
       const user = await User.findById({ _id: decoded.userId });
+      // Check if the user has the required role
       if (!user?.roles?.includes(role)) {
         return res.status(403).json({ error: "Access denied" });
       }
-
+      // Attach userId to req.locals for future use
       req.locals = { currentUserId: user._id };
-      next(); // User has the required role, proceed to the next middleware
+      // User has the required role, proceed to the next middleware
+      next();
     } catch (err) {
-      console.log(err);
+      //console.log(err);
+      // Token verification failed, return Token is not valid
       res.status(401).json({ error: "Token is not valid" });
     }
   };
